c65b4c6c623460cc2f5b7d438af3c4a0ac338ab6
ziyuwan <alex_wan@sjtu.edu.cn>
2025-05-23 00:20:15 +0800

    init

---
+++ README.md
@@ -0,0 +1,105 @@
+# ReMA Project
+This is an open-source framework for Multi-Agent Multi-Turn Reinforcement Learning, i.e. **Re**inforced **M**ulti-**A**gents (ReMA).
+
+## Overview
+![ReMA](./assets/demo.png)
+We implement the paper *Learning to Meta-think for LLMs with Multi-Agent Reinforcement Learning* as an initial example.
+In this work, we decouples the reasoning process into two hierarchical agents: a high-level meta-thinking agent responsible for generating strategic oversight and plans, and a low-level reasoning agent for detailed executions.
+
+We support two types of training:
+
+![Training](./assets/train.png)
+
+### ReMA with Separate Parameters
+In this mode, the high-level agent (meta-thinking) and the low-level agent (reasoning) are parameterized separately. The high-level agent is frozen while the low-level agent is trainable, or vice versa. This allows for independent optimization of meta-thinking and reasoning abilities.
+
+### ReMA with Shared Parameters
+In this mode, both the high-level and low-level agents share the same set of parameters. The training is performed end-to-end, and feedback is used to update the shared parameters jointly. This enables unified learning of both meta-thinking and reasoning within a single model.
+You can choose the training mode according to your research needs. For more details, please refer to the documentation and the code examples in the src directory.
+
+
+### Turn-level Ratio 
+
+![Turn-level Ratio](./assets/turn_ratio.png)
+
+We also proposed a turn-level ratio for clip and loss averaging, which is more efficient and effective than token-level ratio.
+
+
+## Installation
+we recommend using cuda12.4 + torch2.6 + python3.10.
+```bash
+conda create -n rema python=3.10
+conda activate rema
+# install flash-attn (faster)
+# wget https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.6cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
+# pip install flash_attn-2.7.4.post1+cu12torch2.6cxx11abiFALSE-cp310-cp310-linux_x86_64.whl --no-build-isolation
+
+# install flash-attn
+pip install flash-attn==2.7.4.post1 --no-build-isolation
+
+# for sft
+cd src/360-LLaMA-Factory
+pip install -e .
+
+# for RL
+cd src/verl
+pip install -e .
+
+# update libs
+pip install -r requirements.txt
+```
+
+## Training
+
+
+### How to run scripts
+To run the training, you can see `scripts/rl/rema/example_7b.sh` as an example.
+```bash
+bash scripts/rl/rema/example_7b.sh
+```
+
+key arguments:
+- Rollout
+    - `data.max_prompt_length`: max length of prompt, **for each turn's input**
+    - `data.max_response_length`: max length of response **per turn**
+    - `actor_rollout_ref.rollout.stop_when_truncated=True`: whether to stop when truncated, i.e. last agent's response is stopped by length
+
+- Training:
+    - `actor_rollout_ref.actor.clip_mode`: ratio clip mode, `"turn"` or `"batch"`
+    - `actor_rollout_ref.actor.agg_mode`: ratio aggregation mode, `"trajectory"` or `"batch"` or `"turn"`
+
+- For Separated Trainer:
+    - `algorithm.switch_agent.enable=True`: whether to use separated trainer
+    - `algorithm.switch_agent.freq=10`: switch agent frequency
+    - `algorithm.switch_agent.model_paths`: model paths of two agents, **Previous `actor_rollout_ref.model.path` will be ignored**
+
+see `src/verl/verl/rema_trainer/config/ppo_trainer.yaml` and `src/verl/verl/rema_separated_trainer/config/ppo_trainer.yaml` for more details.
+
+
+
+## Evaluation
+A very easy way is to set
+```
++trainer.val_before_train=True \
++trainer.val_only=True \
+```
+in the training scripts.
+
+
+## Basic Results
+![Basic Results](./assets/results.png)
+- Multi-agent Multi-turn RL training is still highly sensitive and often unstable(collapse)
+- Turn-level ratio is more efficient and effective than token-level ratio.
+
+
+
+## Citation
+If you find this work useful, please consider citing:
+```
+@article{wan2025rema,
+  title={Rema: Learning to meta-think for llms with multi-agent reinforcement learning},
+  author={Wan, Ziyu and Li, Yunxiang and Song, Yan and Wang, Hanjing and Yang, Linyi and Schmidt, Mark and Wang, Jun and Zhang, Weinan and Hu, Shuyue and Wen, Ying},
+  journal={arXiv preprint arXiv:2503.09501},
+  year={2025}
+}
+```
---
+++ src/verl/verl/workers/reward_manager/rema.py
@@ -0,0 +1,174 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from functools import partial
+from typing import Dict
+
+from tqdm import tqdm
+from verl import DataProto
+from verl.utils.reward_score import _default_compute_score
+import torch
+from pebble import ProcessPool
+from concurrent.futures import TimeoutError
+from math_verify.errors import TimeoutException
+
+def compute_score_fn(compute_score, params):
+    data_source, response, ground_truth, extra_info = params
+    return compute_score(data_source, response, ground_truth, extra_info)
+
+class ReMARewardManager:
+    """The reward manager.
+    """
+
+    def __init__(self, tokenizer, num_examine, compute_score=None) -> None:
+        self.tokenizer = tokenizer
+        self.num_examine = num_examine  # the number of batches of decoded responses to print to the console
+        self.compute_score = compute_score or _default_compute_score
+
+    def verify(self, data):
+        scores = []
+        for i in range(len(data)):
+            data_item = data[i]  # DataProtoItem
+
+            prompt_ids = data_item.batch['prompts']
+
+            prompt_length = prompt_ids.shape[-1]
+
+            valid_prompt_length = data_item.batch['attention_mask'][:prompt_length].sum()
+            valid_prompt_ids = prompt_ids[-valid_prompt_length:]
+
+            response_ids = data_item.batch['responses']
+            valid_response_length = data_item.batch['attention_mask'][prompt_length:].sum()
+            valid_response_ids = response_ids[:valid_response_length]
+
+            # decode
+            prompt_str = self.tokenizer.decode(valid_prompt_ids, skip_special_tokens=True)
+            response_str = self.tokenizer.decode(valid_response_ids, skip_special_tokens=True)
+
+            ground_truth = data_item.non_tensor_batch['reward_model']['ground_truth']
+
+            data_source = data_item.non_tensor_batch['data_source']
+
+            extra_info = data_item.non_tensor_batch.get('extra_info', None)
+
+            score = self.compute_score(
+                data_source=data_source,
+                solution_str=response_str,
+                ground_truth=ground_truth,
+                extra_info=extra_info,
+            )
+            scores.append(score)
+        data.batch['acc'] = torch.tensor(scores, dtype=torch.float32, device=prompt_ids.device)
+        return scores
+
+    def __call__(self, data: DataProto)-> Dict[str, torch.Tensor]:
+        """We will expand this function gradually based on the available datasets"""
+
+        # If there is rm score, we directly return rm score. Otherwise, we compute via rm_score_fn
+        if 'rm_scores' in data.batch.keys():
+            return data.batch['rm_scores']
+        
+        batch_size = len(data)
+        max_num_turns = data.meta_info['max_num_turns']
+
+        
+        agent_roles = data.meta_info['agent_roles']
+        reward_tensor_map = {
+            f'{role}_turn_level_reward': torch.zeros(batch_size, max_num_turns, dtype=torch.float32) for role in agent_roles
+        }
+        
+        already_print_data_sources = {}
+
+        params = [
+            (data[i].non_tensor_batch['data_source'],
+             data[i].non_tensor_batch['response'],
+             data[i].non_tensor_batch['reward_model']['ground_truth'],
+             data[i].non_tensor_batch.get('extra_info', None),
+             )
+            for i in range(len(data))
+        ]
+
+        scores = []
+        with ProcessPool(max_workers=1) as pool:
+            future = pool.map(partial(compute_score_fn, self.compute_score), params, timeout=10)
+            iterator = future.result()
+            with tqdm(total=len(data), desc="Computing scores") as pbar:
+                while True:
+                    try:
+                        result = next(iterator)
+                        scores.append(result)
+                    except TimeoutError:
+                        print('Time Out')
+                        scores.append(0.0)
+                    except TimeoutException:
+                        print('Math verify internal timeout')
+                        scores.append(0.0)
+                    except StopIteration:
+                        break
+                    except Exception as e:
+                        print(f"Error: {e}")
+                        raise e
+                    pbar.update(1)
+        
+        assert len(scores) == len(data)
+        accuracy = torch.tensor(scores, dtype=torch.float32) # bsz
+        reward_tensor_map['acc'] = accuracy
+        for i in range(len(data)):
+            data_item = data[i]  # DataProtoItem
+            response_str = data_item.non_tensor_batch['response']
+            ground_truth = data_item.non_tensor_batch['reward_model']['ground_truth']
+            data_source = data_item.non_tensor_batch['data_source']
+            # extra_info = data_item.non_tensor_batch.get('extra_info', None)
+            # score = self.compute_score(
+            #     data_source=data_source,
+            #     solution_str=response_str,
+            #     ground_truth=ground_truth,
+            #     extra_info=extra_info,
+            # )
+            score = scores[i]
+            
+            num_turns = data_item.non_tensor_batch['num_turns']
+            
+            for role in agent_roles:
+                if data_item.meta_info['mask_unfinished_reward']:
+                    turn_finished = data_item.batch[f'{role}_turn_finished'].item()
+                    # if conversation is not finised normally, i.e. with ['FINISH']
+                    #  the reward should be zero.
+                    score = score if turn_finished == 0 else 0.0
+                else:
+                    if max_num_turns == 1 and 'boxed' in response_str:
+                        if role == 'meta-thinking':
+                            score -= 0.25
+                        elif role == 'reasoning':
+                            score += 0.25
+                        else:
+                            raise ValueError(f"Unknown {role=}")
+                reward_tensor_map[f'{role}_turn_level_reward'][i, num_turns - 1] = score
+
+            if data_source not in already_print_data_sources:
+                already_print_data_sources[data_source] = 0
+
+            if already_print_data_sources[data_source] < self.num_examine:
+                prompt_str = data_item.non_tensor_batch['question']
+                padded_history = data_item.non_tensor_batch['history']
+                history = padded_history[:num_turns * 2]
+                already_print_data_sources[data_source] += 1
+                print("[question]", prompt_str)
+                print("[ground_truth]", ground_truth)
+                print("[answer]", response_str)
+                print("[score]", score)
+                print("[history]", history)
+
+        # Return both reward tensors in a dictionary
+        return reward_tensor_map
